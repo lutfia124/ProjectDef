b9c8776cfce69f142747f1718823c26442af9467
HasnaFayza <84272413+HasnaFayza@users.noreply.github.com>
2021-06-02 22:55:43 +0700

    Create tampilancash.js

---
+++ tampilancash.js
@@ -0,0 +1,4 @@
+function kecash(){ //untuk masuk ke tampilan cash
+		document.getElementById("kasir").style.display = "block";
+		document.getElementById('metodepembayaran').style.display = "none";
+	}
